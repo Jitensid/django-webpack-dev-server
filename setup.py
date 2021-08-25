264bde300d1e8d885f817b3fe04df95c587ab7b8
Jiten Sidhpura <jitensidhpura2000@gmail.com>
2021-08-25 23:08:29 +0530

    Modified Setup File

--- setup.py
+++ setup.py
@@ -2,19 +2,16 @@ from distutils.core import setup
 
 package_description = 'A Django app to create configuration files for frontend Javascript framework.'
 
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name='django-webpack-dev-server',
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     author='Jiten Sidhpura',
     author_email='jitensidhpura2000@gmail.com',
     description=package_description,
-    long_description=long_description,
+    long_description=open('README.rst').read(),
     url='https://github.com/Jitensid/django-webpack-dev-server',
-    download_url='https://github.com/Jitensid/django-webpack-dev-server/archive/refs/tags/0.0.2.tar.gz',
+    download_url='https://github.com/Jitensid/django-webpack-dev-server/archive/refs/tags/0.0.3.tar.gz',
     requires=['requests', 'progressbar2'],
     keywords=['django', 'webpack', 'webpack_dev_server', 'react'],
     classifiers=[
