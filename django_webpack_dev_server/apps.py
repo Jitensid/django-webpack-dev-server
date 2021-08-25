fb18c511899e14c8b5cdca70caeb3926da33caf0
Jiten Sidhpura <jitensidhpura2000@gmail.com>
2021-08-25 20:04:43 +0530

    Added the package

---
+++ django_webpack_dev_server/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoWebpackDevServerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_webpack_dev_server'
